e9a6f3978887fa38dfcdc20bf20e57a20f2c88a9
Suryateja K <kutalasuryateja@gmail.com>
2022-01-12 16:43:00 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('hiii')
+
+
+# In[1]:
+
+
+#import pandas as pd
+
+
+# In[2]:
+
+
+print('Pandas imported')
+
+
+# In[3]:
+
+
+print('Can plrint multiple lines')
+print('line 1')
+print('line 2')
+print('line 3')
+
+
+# In[ ]:
+
+
+
+
